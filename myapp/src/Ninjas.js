7f0aba3bf53d714c758daa118019c0b94185c697
Tony Watkins <tony@llifion.net>
2020-10-27 20:48:09 +0000

    Alternative to evaluation before the component return body - removes the need for an intermediate constant by evaluating within the returned jsx elements

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -16,20 +16,20 @@ const Ninjas = ({ ninjas }) => {
     //     }
     // });
 
-    // Ternary operator example
-    const ninjaList = ninjas.map(ninja => {
-        return ninja.age > 20 ? (
-            <div className="ninja" key={ninja.id}>
-                <div>Name: {ninja.name}</div>
-                <div>Age: {ninja.age}</div>
-                <div>Belt: {ninja.belt}</div>
-            </div>
-        ) : null
-    })
 
     return (
         <div className="ninja-list">
-            { ninjaList}
+            {
+                ninjas.map(ninja => {
+                    return ninja.age > 20 ? (
+                        <div className="ninja" key={ninja.id}>
+                            <div>Name: {ninja.name}</div>
+                            <div>Age: {ninja.age}</div>
+                            <div>Belt: {ninja.belt}</div>
+                        </div>
+                    ) : null;
+                })
+            }
         </div>
     );
 
