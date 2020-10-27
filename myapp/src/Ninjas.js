6f5b4fb9a9ea92b08c47fe9e18f5e06f43a43456
Tony Watkins <tony@llifion.net>
2020-10-27 11:41:32 +0000

    Alternative to exclude ninjas less than 20 via an if statement (not as clean)

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -3,14 +3,18 @@ import React from 'react'
 const Ninjas = ({ ninjas }) => {
 
     // const { ninjas } = this.props;
-    const ninjaList = ninjas.filter(ninja => ninja.age > 20).map(ninja => {
-        return (
-            <div className="ninja" key={ninja.id}>
-                <div>Name: {ninja.name}</div>
-                <div>Age: {ninja.age}</div>
-                <div>Belt: {ninja.belt}</div>
-            </div>
-        )
+    const ninjaList = ninjas.map(ninja => {
+        if (ninja.age > 20) {
+            return (
+                <div className="ninja" key={ninja.id}>
+                    <div>Name: {ninja.name}</div>
+                    <div>Age: {ninja.age}</div>
+                    <div>Belt: {ninja.belt}</div>
+                </div>
+            )
+        } else {
+            return null;
+        }
     });
 
     return (
