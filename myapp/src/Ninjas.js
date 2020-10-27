f5ad91bd969495d966f1da49b2ed2544f5eba8f5
Tony Watkins <tony@llifion.net>
2020-10-27 11:51:11 +0000

    Another way to achieve conditional output is with the ternary operator

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -2,20 +2,30 @@ import React from 'react'
 
 const Ninjas = ({ ninjas }) => {
 
-    // const { ninjas } = this.props;
+    // const ninjaList = ninjas.map(ninja => {
+    //     if (ninja.age > 20) {
+    //         return (
+    //             <div className="ninja" key={ninja.id}>
+    //                 <div>Name: {ninja.name}</div>
+    //                 <div>Age: {ninja.age}</div>
+    //                 <div>Belt: {ninja.belt}</div>
+    //             </div>
+    //         )
+    //     } else {
+    //         return null;
+    //     }
+    // });
+
+    // Ternary operator example
     const ninjaList = ninjas.map(ninja => {
-        if (ninja.age > 20) {
-            return (
-                <div className="ninja" key={ninja.id}>
-                    <div>Name: {ninja.name}</div>
-                    <div>Age: {ninja.age}</div>
-                    <div>Belt: {ninja.belt}</div>
-                </div>
-            )
-        } else {
-            return null;
-        }
-    });
+        return ninja.age > 20 ? (
+            <div className="ninja" key={ninja.id}>
+                <div>Name: {ninja.name}</div>
+                <div>Age: {ninja.age}</div>
+                <div>Belt: {ninja.belt}</div>
+            </div>
+        ) : null
+    })
 
     return (
         <div className="ninja-list">
