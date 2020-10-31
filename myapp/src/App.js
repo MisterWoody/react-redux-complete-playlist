b81516c58c29e0247ca358be6198f22f7f55f03c
Tony Watkins <tony@llifion.net>
2020-10-31 16:34:26 +0000

    Component did update lifecycle hook fires whn there was a change to the state or props

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -31,6 +31,11 @@ class App extends Component {
   componentDidMount() {
     console.log('Component mounted');
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('Component updated');
+    console.log(prevProps, prevState);
+  }
   render() {
     return (
       <div className="App">
