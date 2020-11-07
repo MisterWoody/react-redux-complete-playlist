61d329b01de0cc4f11f2245808805a6f46fb3655
Tony Watkins <tony@llifion.net>
2020-11-07 16:15:57 +0000

    Import link from react router allowing us to wrap the post title to a relavent url with a dynamic id property

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,35 +1,38 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 class Home extends Component {
   state = {
     posts: []
   }
-  componentDidMount(){
+  componentDidMount() {
     axios.get('https://jsonplaceholder.typicode.com/posts/')
       .then(res => {
         console.log(res);
         this.setState({
-          posts: res.data.slice(0,10)
+          posts: res.data.slice(0, 10)
         });
       })
   }
-  render(){
+  render() {
     const { posts } = this.state
     const postList = posts.length ? (
       posts.map(post => {
         return (
           <div className="post card" key={post.id}>
             <div className="card-content">
-              <span className="card-title">{post.title}</span>
+              <Link to={'/' + post.id}>
+                <span className="card-title">{post.title}</span>
+              </Link>
               <p>{post.body}</p>
             </div>
           </div>
         )
       })
     ) : (
-      <div className="center">No posts to show</div>
-    );
+        <div className="center">No posts to show</div>
+      );
 
     return (
       <div>
