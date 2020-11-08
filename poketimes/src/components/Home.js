8e3121cd74eaf4289c0429a33b1c6d145fd60e01
Tony Watkins <tony@llifion.net>
2020-11-08 11:04:39 +0000

    Demonstration of how to use images in react - they need to be imported and used as dynamic tags

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import Pokeball from '../pokeball.png'
 
 class Home extends Component {
   state = {
     posts: []
   }
-  componentDidMount(){
+  componentDidMount() {
     axios.get('https://jsonplaceholder.typicode.com/posts/')
       .then(res => {
         console.log(res);
         this.setState({
-          posts: res.data.slice(0,10)
+          posts: res.data.slice(0, 10)
         });
       })
   }
-  render(){
+  render() {
     const { posts } = this.state
     const postList = posts.length ? (
       posts.map(post => {
         return (
           <div className="post card" key={post.id}>
+            <img src={Pokeball} alt="A pokeball" />
             <div className="card-content">
               <Link to={'/' + post.id}>
                 <span className="card-title red-text">{post.title}</span>
@@ -31,8 +33,8 @@ class Home extends Component {
         )
       })
     ) : (
-      <div className="center">No posts to show</div>
-    );
+        <div className="center">No posts to show</div>
+      );
 
     return (
       <div>
